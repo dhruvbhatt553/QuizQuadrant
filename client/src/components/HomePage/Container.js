0a99a6888cd03c39f0f31b772ace2aa307fa4964
Bhatt <dhruvbhatt553@gmail.com>
2024-03-02 15:19:25 +0530

    Front page complete

--- client/src/components/HomePage/Container.js
+++ client/src/components/HomePage/Container.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import data from './data';
+import Subject from './Subject';
 
 export default function Container() {
     return (
         <div className='mt-3'>
             <span className='flex ms-2 text-orange-500 font-bold text-2xl'>Welcome to our website name </span>
             <span className='flex ms-2 font-light text-xl'>Practice mcqs,msqs topic wise and design tests effectively !</span>
+            
+            <div className='flex flex-wrap justify-center px-10 gap-10 py-4'>
+
+        
+            {
+                data.map((item)=><Subject subject={item}/>)
+            }
+            </div>
 
         </div>
     );
--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import NavBar from './NavBar';
 import Container from './Container';
 
 
 export default function HomePage() {
     return (
         <div>
-            <NavBar/>
             <Container/>
         </div>
     );
