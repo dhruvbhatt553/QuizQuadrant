b9965e77768777199ab497238ede12eebaa554dd
Bhatt <dhruvbhatt553@gmail.com>
2024-03-13 15:41:34 +0530

    Update navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 export default function NavBar() {
     return (
-        <div className='bg-slate-300 h-16 flex italic font-semibold text items-center justify-between px-4'>
-            Website-Name
-            <div >
+        <div className='bg-slate-300 h-16 flex  font-semibold text items-center justify-between px-4'>
+            <p className='text-2xl'>Quiz Quadrant</p>
+            <div className='flex align-middle gap-x-3'>
+                <button className='border border-blue-800 p-2'>
+                    Make Test/ Mock test
+                </button>
                 <button >
 
                     <img src="/images/log-in.png"
